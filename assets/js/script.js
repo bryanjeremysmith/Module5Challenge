8490a44404c95db80b118785a3777355e09e419e
BJ Smith <bryanjeremysmith@gmail.com>
2022-11-17 21:33:41 -0800

    dynamically building agenda and cleaning up

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,57 +2,69 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  var firstHour = 9;
+  var lastHour = 18;
 
-  var saveButtons = $("[class^='btn saveBtn col-2 col-md-1']");
-  for(var i = 0; i < saveButtons.length; i++){
-    saveButtons.eq(i).on("click", function(){
-      let appointment = $(this).parent().children().eq(1).val().trim();
-      if(appointment != ""){
-        localStorage.setItem($(this).parent().attr('id'), appointment);
-        $("#statusInformation").text("Updated " + $(this).parent().children().eq(0).text() + " agenda!");
-        
-        setTimeout(function() {
-          $("#statusInformation").text("");
-        }, 2000);
+  //Builds the agenda from the firstHour to the lastHour, dynamically using jQuery
+  function BuildAgenda(firstHour, lastHour){
+    for(var index = firstHour; index < lastHour; index++){
+      var hourDiv = $('<div>').attr("id", "hour-" + index).addClass('row time-block');
+
+      var colDiv = $('<div>').addClass("col-2 col-md-1 hour text-center py-3");
+      if(index < 12){
+        colDiv.text(index + "AM");
       }
+      else if (index == 12)
+      {
+        colDiv.text("12PM");
+      }
+      else
+      {
+        colDiv.text((index - 12) + "PM");
+      }
+
+      hourDiv.append(colDiv);
+
+      hourDiv.append($('<textarea>').addClass("col-8 col-md-10 description").attr("rows", 3));
+
+      hourDiv.append($('<button>').addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").append($('<i>').addClass("fas fa-save").attr("aria-hidden", "true")));
+
+      $("#agendaDiv").append(hourDiv);
+    }
+
+    //Listens for the on click of a button
+    $("[class^='btn saveBtn col-2 col-md-1']").on("click", function (){
+      let appointment = $(this).parent().children().eq(1).val().trim();
+        if(appointment != ""){
+          localStorage.setItem($(this).parent().attr('id'), appointment);
+          $("#statusInformation").text("Updated " + $(this).parent().children().eq(0).text() + " appointment to local storage. ✔️");
+          
+          setTimeout(function() {
+            $("#statusInformation").text("");
+          }, 2000);
+        }
     });
   }
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
-  function applyPastPresentFutureClass(){
+  //Applies the past, present and future class by comparing hte id to the current hour.
+  function ApplyPastPresentFutureClass(){
     var now = dayjs();
     var hour = now.hour();
-    console.log(hour);
 
-    for(var i = 9; i < hour; i++ ){
+    for(var i = firstHour; i < hour; i++ ){
       $("#hour-"+i).addClass("past");
     }
 
-    if(hour >= 9 && hour < 17){
+    if(hour >= firstHour && hour < lastHour){
       $("#hour-"+i).addClass("present");
     }
 
-    for(var i = hour + 1; i < 17; i++){
+    for(var i = hour + 1; i < lastHour; i++){
       $("#hour-"+i).addClass("future");
     }
   }
 
-  applyPastPresentFutureClass();
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  //Loads the agenda information from local storage to the agenda.
   function LoadAgendaFromLocalStorage(){
     var agenda = $("[class^='col-8 col-md-10 description']");
     for(var i = 0; i < agenda.length; i++){
@@ -60,24 +72,19 @@ $(function () {
     }
   }
 
-  // TODO: Add code to display the current date in the header of the page.
-
   //Helper function to get the ordinal from the current date, 1-31.
   function getOrdinal(currDate){
     var ordinal = currDate;
-    if (currDate == 1 || currDate == 21 || currDate ==31)
-    {
+    if (currDate == 1 || currDate == 21 || currDate ==31){
       ordinal += "st";
     }
     else if (currDate == 2 || currDate == 22){
       ordinal += "nd";
     }
-    else if (currDate == 3 || currDate == 23)
-    {
+    else if (currDate == 3 || currDate == 23){
       ordinal += "rd";
     }
-    else
-    {
+    else{
       ordinal += "th";
     }
     return ordinal;
@@ -99,10 +106,29 @@ $(function () {
 
   //Function to display the current date in header.
   function DisplayCurrentDateInHeader(){
-    var now = dayjs();
-    $("#currentDay").text(now.format("dddd, MMMM") + " " + getOrdinal(now.date()));
+    
+    //Future work, doesn't appear to work with this version of jQuery
+    //var advancedFormat = require('dayjs/plugin/advancedFormat')
+    //dayjs.extend(advancedFormat)
+    //$("#currentDay").text(now.format("dddd, MMMM Do"));
+    
+    setInterval(function(){
+      var now = dayjs();
+      $("#currentDay").text(now.format("dddd, MMMM") + " " + getOrdinal(now.date()) + ", " + now.format("YYYY"));
+
+    }, 1000);
+    
   }
 
+  //Displays the Current Date In Header
   DisplayCurrentDateInHeader();
+
+  //Builds the agenda
+  BuildAgenda(firstHour, lastHour);
+
+  //Loads any previously saved information from local storage to the agenda
   LoadAgendaFromLocalStorage();
+
+  //Applies the past, present, and future classes
+  ApplyPastPresentFutureClass();
 });
